caa80aaeedb02bf3c031f2bf29ded03f9c5fbcc1
EduardoReis7 <53585326+EduardoReis7@users.noreply.github.com>
2020-12-09 00:13:24 -0300

    add massa de dados dos associados na execucao do sistema

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+INSERT INTO associado (cod_associado, cpf, nome) VALUES (1, '33094284000', 'Marcelo');
+INSERT INTO associado (cod_associado, cpf, nome) VALUES (2, '11250932009', 'Eduardo');
+INSERT INTO associado (cod_associado, cpf, nome) VALUES (3, '63799450084', 'Jeferson');
+INSERT INTO associado (cod_associado, cpf, nome) VALUES (4, '70217484000', 'Elisa');
+INSERT INTO associado (cod_associado, cpf, nome) VALUES (5, '92860351019', 'Maria');
+INSERT INTO associado (cod_associado, cpf, nome) VALUES (6, '74424749002', 'Silvia');
+INSERT INTO associado (cod_associado, cpf, nome) VALUES (7, '28624927013', 'Fernando');
+INSERT INTO associado (cod_associado, cpf, nome) VALUES (8, '11488089060', 'Carolina');
+INSERT INTO associado (cod_associado, cpf, nome) VALUES (9, '30201661012', 'Mateus');
+INSERT INTO associado (cod_associado, cpf, nome) VALUES (10, '77431567022', 'Matilda');
